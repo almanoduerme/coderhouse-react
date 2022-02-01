ca00b2b99ee9e54ea8dcdd1e86da49d1c1ff75b1
“Nahuel <ngomezdw@gmail.com>
2022-01-31 22:39:41 -0300

    Created NavBar component

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./NavBar.css";
+import { GoThreeBars } from "react-icons/go";
+
+const NavBar = () => {
+  return (
+    <header className="header">
+      <div className="header-logo">
+        <img className="logo" src="./img/logo2.png" alt="logo" />
+        <span> TRUSTINESS STORE</span>
+      </div>
+      <GoThreeBars className="bars" />
+      <nav className="navbar">
+        <ul className="navbar-nav">
+          <li>
+            <a className="link" href="/">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="link" href="/">
+              Products
+            </a>
+          </li>
+          <li>
+            <a className="link" href="/">
+              About us
+            </a>
+          </li>
+          <li>
+            <a className="link" href="/">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default NavBar;
